e1e2744aa4bed047a48f02ff62925871b8c6454b
Jesse Peters <jessepeters@gmail.com>
2024-09-19 21:05:54 -0500

    Added to line 41 in main

--- main.py
+++ main.py
@@ -37,3 +37,5 @@ print(findARegx.sub('new', text)) #Replaces each instance of the provided patter
 ##### New Features #####
 def add_two(num):
     return num + 2
+
+# I am adding this line to the main after the fact
